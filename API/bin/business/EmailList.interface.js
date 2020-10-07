f5a27fbed8d9a0eb485dc77e1a8ef3a2dbc0cf34
José Cabeda <jecabeda@gmail.com>
2020-10-07 18:16:26 +0000

    Initial commit

---
+++ API/bin/business/EmailList.interface.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var EmailList = /** @class */ (function () {
+    function EmailList() {
+    }
+    return EmailList;
+}());
+exports.EmailList = EmailList;
+//# sourceMappingURL=EmailList.interface.js.map
